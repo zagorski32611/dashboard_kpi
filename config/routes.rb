b95321cd162b6107c2dc3e551a0238acfec6413a
zagorski32611 <j.j.zagorski@gmail.com>
2017-10-08 15:35:59 -0400

    added routes, devise stuff, migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+  sessions: 'users/sessions'
+  }
+  root 'static_pages#home'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
 end
