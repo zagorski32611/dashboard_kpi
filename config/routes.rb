0a94489248fcb848dd2261f3da94b715e8724ec6
zagorski32611 <j.j.zagorski@gmail.com>
2017-10-14 20:14:05 -0400

    added a bunch of stuff - SORRY

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
+  get '/dashboard', to: 'static_pages#dashboard'
 end
