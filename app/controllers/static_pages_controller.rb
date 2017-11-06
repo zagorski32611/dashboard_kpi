2eb076ca45e93a3ec542f95faeb13b121b9ec719
zagorski32611 <j.j.zagorski@gmail.com>
2017-11-06 18:24:49 -0500

    add the other methods!

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,17 +19,11 @@ class StaticPagesController < ApplicationController
   end
 
   def dashboard
-    #include HTTParty
     @auth = { :username => 'fea750e8cae545ca89f1fcf34ab972cb',
              :password => 'e8dd80d5095540049e89f9f9f64b2b2a' }
     @data = HTTParty.get("http://avatarfleet.happyfox.com/api/1.1/json/tickets/?size=50&page=1",
                              :basic_auth => @auth, :headers =>{'Content-Type' => 'application/json'})
     @tickets = JSON.parse(@data.body)
-
-    #@counters = HappyFoxAPI.count_each_status(@tickets, "Open", "New", "Closed", "On Hold", "Unanswered")
-
-    #@unresponded = HappyFoxAPI.count_unresponded(@tickets) # doesn't work
-
   end
 
   def show(tickets, *statuses)
@@ -43,8 +37,33 @@ class StaticPagesController < ApplicationController
      return @status_counters
   end
   helper_method :show
-    #@counters = HappyFoxAPI.count_each_status(@tickets, "Open", "New", "Closed", "On Hold", "Unanswered")
-    #@unresponded = HappyFoxAPI.count_unresponded(@tickets) # doesn't work
+
+  def count_unresponded(tickets)
+    true_counter = 0
+    false_counter = 0
+    tickets["data"].each do |tix|
+      if tix["unresponded"] == false
+        false_counter += 1
+      else true_counter += 1
+      end
+    end
+    return "There are #{true_counter} tickets without a response"
+    return "There are #{false_counter} ticket with a response"
+  end
+  helper_method :count_unresponded
+
+  def user_count(users)
+    user_count = Hash.new(0)
+    @tickets["data"].each do |users|
+        if users["user"]["name"] == users["user"]["name"]
+          user_count[users["user"]["name"]] += 1
+        end
+      end
+      return user_count
+  end
+  helper_method :user_count
+
+
 
 
 end
